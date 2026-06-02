398ddb6790c3b11b3b393cb55fea805e4a6528af
raccoon8right <gpepito408@gmail.com>
2026-06-02 10:11:19 -0400

    feat: add express-validator to pedido routes

---
+++ backend/middlewares/pedido.validator.js
@@ -0,0 +1,8 @@
+import { body, validationResult } from 'express-validator'
+
+export const validarPedido = [
+    body('cod').notEmpty().withMessage('El campo código es obligatorio'),
+    body('direccion_entrega').notEmpty().withMessage('El campo dirección de entrega es obligatorio'),
+    body('total').notEmpty().withMessage('El campo total es obligatorio')
+        .isFloat({ min: 0 }).withMessage('El campo total debe ser un número positivo')
+]
--- backend/routes/pedido.routes.js
+++ backend/routes/pedido.routes.js
@@ -1,12 +1,14 @@
 import { getPedido, getPedidoPorCod, postPedido, putPedidoPorCod, deletePedidoPorCod } from '../controllers/pedido.controller.js'
+import { validarPedido } from '../middlewares/pedido.validator.js'
+import { validarCampos } from '../middlewares/validarCampos.js'
 import express from 'express'
 
 const routes = express.Router()
 
 routes.get('/', getPedido)
 routes.get('/:cod', getPedidoPorCod)
-routes.post('/', postPedido)
-routes.put('/:cod', putPedidoPorCod)
+routes.post('/', validarPedido, validarCampos, postPedido)
+routes.put('/:cod', validarPedido, validarCampos, putPedidoPorCod)
 routes.delete('/:cod', deletePedidoPorCod)
 
 export default routes
